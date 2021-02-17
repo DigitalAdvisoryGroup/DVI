65ad31f6fb8185e6a0790e5f16e724f3136ab586
Siddharth <siddharth>
2021-02-17 11:55:23 +0530

    [ADD] partner ledger asset API

--- justthis_customization/models/res_partner.py
+++ justthis_customization/models/res_partner.py
@@ -29,3 +29,77 @@ class ResPartner(models.Model):
             return {"status": True, 'base64_content': b64_pdf,'report_name': report_name.get('name')}
         return {'status': False,
                 "msg": "Partner,Fromdate,Todate is required."}
+
+
+    def get_assets_data(self, date_from=False, date_to=False):
+        jounral_id = self.env['account.journal'].sudo().search([('company_id', '=', self.company_id.id)])
+        final_data = []
+        cr_invoice_ids = self.env['account.invoice'].sudo().search([('date_invoice', '>=', date_from),
+                                                             ('date_invoice', '<=', date_to),
+                                                             ('state', 'in', ('open', 'paid')),
+                                                             ('type', '=', 'out_refund'),
+                                                             ('residual', '!=', 0.0),
+                                                             ('journal_id', 'in', jounral_id.ids),
+                                                             ('partner_id', '=', self.id),
+                                                             ('company_id', '=', self.env.user.company_id.id)
+                                                             ])
+        if cr_invoice_ids:
+            for inv in cr_invoice_ids:
+                inv_vals = {
+                    "id": inv.id,
+                    "date": inv.date_invoice,
+                    "x_jt_main1_id": inv.x_jt_main1_id,
+                    "x_jt_main2_id": inv.x_jt_main2_id,
+                    "x_jt_deposit_id": inv.x_jt_deposit_id,
+                    "code": inv.journal_id.code,
+                    "a_code": inv.account_id.code,
+                    "a_name": inv.account_id.name,
+                    "ref": inv.reference,
+                    "move_name": inv.move_id.name,
+                    "name": inv.name,
+                    "state": inv.state,
+                    "debit": inv.residual,
+                    "credit": 0.0,
+                    "balance": inv.residual,
+                    "amount_currency": 0.0,
+                    "currency_id": inv.currency_id,
+                    "currency_code": False,
+                    "progress": 0.0,
+                    "displayed_name": '-'.join(field_name for field_name in (inv.move_id.name, inv.reference, '') if field_name not in (False, None, '', '/')),
+                    "lines": []
+                }
+                final_data.append(inv_vals)
+        payment_ids = self.env['account.payment'].search([('partner_id', '=', self.id),
+                                                          ('state', 'in', ('posted', 'reconciled')),
+                                                          ('payment_type', '=', 'inbound'),
+                                                          ])
+        if payment_ids:
+            unlink_payment_ids = payment_ids.filtered(lambda p: not p.has_invoices)
+            if unlink_payment_ids:
+                for payment in unlink_payment_ids:
+                    payment_vals = {
+                        "id": payment.id,
+                        "date": payment.payment_date,
+                        "x_jt_main1_id": payment.x_jt_main1_id,
+                        "x_jt_main2_id": payment.x_jt_main2_id,
+                        "x_jt_deposit_id": payment.x_jt_deposit_id,
+                        "code": payment.journal_id.code,
+                        "a_code": payment.destination_account_id.code,
+                        "a_name": payment.destination_account_id.name,
+                        "ref": '',
+                        "move_name": payment.name,
+                        "name": payment.name,
+                        "state": payment.state,
+                        "debit": payment.amount,
+                        "credit": 0.0,
+                        "balance": payment.amount,
+                        "amount_currency": 0.0,
+                        "currency_id": payment.currency_id,
+                        "currency_code": False,
+                        "progress": 0.0,
+                        "lines": []
+                    }
+                    final_data.append(payment_vals)
+        return final_data
+
+
